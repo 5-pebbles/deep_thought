52e0c4b574d250a51828d508a9e65fa13f285861
5-pebbles <5-pebble@protonmail.com>
2024-03-05 01:28:36 +0000

    feat(docs): add endpoint to get generated docs.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 use rocket::serde::{Deserialize, Serialize};
-use rocket::{get, launch, routes, serde::json::Json};
+use rocket::{
+    get,
+    http::{ContentType, Status},
+    launch, routes,
+    serde::json::Json,
+};
+use utoipa::{OpenApi, ToSchema};
 
-#[derive(Deserialize, Serialize)]
+// Api
+#[derive(Deserialize, Serialize, ToSchema)]
 #[serde(crate = "rocket::serde")]
 struct Answer {
     pub question: String,
     pub answer: u8,
 }
 
+#[utoipa::path(
+    get,
+    path = "/answer",
+    responses(
+        (status = 200, description = "Success", body = Answer),
+    ),
+    params(
+        ("question" = String, Query, description = "The question to answer"),
+    )
+)]
 #[get("/answer?<question>")]
 fn get_answer(question: Option<String>) -> Json<Answer> {
     Json(Answer {
@@ -18,5 +35,32 @@ fn get_answer(question: Option<String>) -> Json<Answer> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_answer])
+    rocket::build().mount("/", routes![get_answer, docs_yaml])
+}
+
+// Docs
+#[derive(OpenApi)]
+#[openapi(paths(get_answer, docs_yaml), components(schemas(Answer)))]
+struct ApiDoc;
+
+#[utoipa::path(
+    get,
+    path = "/docs.yaml",
+    responses((
+        status = 200,
+        description = "Success",
+        content_type = "application/x-yaml",
+        body = String,
+    ))
+)]
+#[get("/docs.yaml")]
+fn docs_yaml() -> Result<(ContentType, String), (Status, String)> {
+    let openapi = ApiDoc::openapi();
+    let yaml = serde_yaml::to_string(&openapi).map_err(|e| {
+        (
+            Status::InternalServerError,
+            format!("Failed to parse docs: {}", e),
+        )
+    })?;
+    Ok((ContentType::new("application", "x-yaml"), yaml))
 }
